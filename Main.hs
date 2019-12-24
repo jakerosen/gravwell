c7cbe8e40797cdd283ee789dfc9d1967fce76b6b
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-12-24 14:09:33 -0500

    Main stub

--- Main.hs
+++ Main.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import Data.Foldable (for_)
+
+
 main :: IO ()
-main = pure ()
+main = do
+  putStrLn "Gravwell"
+
+  loop initialGame
+
+loop :: Game -> IO ()
+loop game = do
+  displayGame game
+  if gameOver game
+    then putStrLn "Game over!"
+    else loop' game
+
+loop' :: Game -> IO ()
+loop' game = do
+  case gameState game of
+    (game', outputs) -> do
+      for_ outputs displayOutput
+      _ <- getLine
+      loop game'
+
+type Game = ()
+initialGame = ()
+displayGame = print
+displayOutput = print @()
+gameOver = const False
+gameState = const ((), [()])
